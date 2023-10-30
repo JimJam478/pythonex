8f849a14d8c43f483881b1f7da44c661cd158223
JimJam47 <allensreebinoj2010@gmail.com>
2023-10-30 13:11:48 +0530

    Changes the order of the condition statements in the fizzbizz function
    Changed the order in the if function to correct the fizzbizz error.

--- pyex1.py
+++ pyex1.py
@@ -1,11 +1,11 @@
 def fizzbizz(n):
     for i in range (1,n+1):
-        if i % 3 == 0:
-            print('fizz')
+        if i % 15 == 0:
+            print('fizzbizz')
         elif i%5 ==0:
             print('buzz')
-        elif i%3 == 0 and i%5 == 0:
-            print('fizzbizz')
+        elif i%3 == 0:
+            print('fizz')
         else:
             print(i)
 
