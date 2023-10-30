587b68801d566e350557ba8a19d8116964e868ff
JimJam47 <allensreebinoj2010@gmail.com>
2023-10-30 13:03:53 +0530

    Implements a fizzbizz function
    Added a function to print a list of numbers which:
    	a) prints fizz when the number is divisible by 3
    	b) prints bizz when the number is divisible by 5
    	c) prints fizzbizz when the number is divisible by 15

---
+++ pyex1.py
@@ -0,0 +1,13 @@
+def fizzbizz(n):
+    for i in range (1,n+1):
+        if i % 3 == 0:
+            print('fizz')
+        elif i%5 ==0:
+            print('buzz')
+        elif i%3 == 0 and i%5 == 0:
+            print('fizzbizz')
+        else:
+            print(i)
+
+n = int(input('Enter upperlimit: '))
+fizzbizz(n)
