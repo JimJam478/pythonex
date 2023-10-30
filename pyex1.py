377781e6e27220735059be57a46acd57320fabb0
Allen <allensreebinoj2010@gmail.com>
2023-10-30 17:08:20 +0530

    Implements a freq function
    Added a function which counts the charectors in a string and creates a dictionary with the
    charector as its key and its count as the value.

--- pyex1.py
+++ pyex1.py
@@ -35,3 +35,15 @@ def panagram(s):
 s = input('Enter the string: ')    
 print(panagram(s))
 
+
+def freq(s):
+    dict = {}
+    for i in s:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    return dict 
+
+print(freq('jishnu fundajjhj')) 
+
