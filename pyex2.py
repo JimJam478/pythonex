7724fb297e65e64c0542b42c6a4f38bef8f8b60f
Allen <allensreebinoj2010@gmail.com>
2023-11-01 17:06:08 +0530

    Adds division operation in postfix evaluation

--- pyex2.py
+++ pyex2.py
@@ -1,7 +1,7 @@
 def evaluate(string):
     stack = []
     for i in string:
-        if i in ['+','-','*']:
+        if i in ['+','-','*','/']:
             op1 = stack.pop()
             op2 = stack.pop()
             if i == '+':
@@ -10,7 +10,7 @@ def evaluate(string):
                 stack.append(op2-op1)
             elif i == '*':
                 stack.append(op2*op1)
-            elif i = '/':
+            elif i == '/':
                 stack.append(op2//op1)
         else:
             stack.append(int(i))    
