26491b443c80ad9bd7ca84817915a856b267b7c5
Allen <allensreebinoj2010@gmail.com>
2023-11-01 16:59:30 +0530

    GREEN: Multiplication operation in postflix evaluation

--- pyex2.py
+++ pyex2.py
@@ -1,16 +1,15 @@
 def evaluate(string):
     stack = []
     for i in string:
-        if i in ['+','-']:
+        if i in ['+','-','*']:
             op1 = stack.pop()
             op2 = stack.pop()
             if i == '+':
                 stack.append(op2+op1)
             elif i =='-':
-                stack.append(op2-op1) 
+                stack.append(op2-op1)
+            elif i == '*':
+                stack.append(op2*op1) 
         else:
             stack.append(int(i))    
     return stack.pop()
-
-
-print(evaluate("34+"))
