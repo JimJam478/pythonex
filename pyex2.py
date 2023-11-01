6a257c47d20e5a6c22fb54a6f6d56ecb679bd345
Allen <allensreebinoj2010@gmail.com>
2023-11-01 16:39:04 +0530

    GREEN: Implements substraction operation in postfix evaluation

--- pyex2.py
+++ pyex2.py
@@ -1,11 +1,13 @@
 def evaluate(string):
     stack = []
     for i in string:
-        if i in ['+']:
-            op2 = stack.pop()
+        if i in ['+','-']:
             op1 = stack.pop()
+            op2 = stack.pop()
             if i == '+':
                 stack.append(op2+op1)
+            elif i =='-':
+                stack.append(op2-op1) 
         else:
             stack.append(int(i))    
     return stack.pop()
