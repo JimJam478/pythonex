00b588a4184c585667049fd5dddf6a5540c5e270
Allen <allensreebinoj2010@gmail.com>
2023-11-01 15:36:19 +0530

    RED: Test failed for addition operation in postfix evaluation

--- test_eval.py
+++ test_eval.py
@@ -3,3 +3,9 @@ from pyex2 import evaluate
 def test_singleoperand():
     assert evaluate("6") == 6
 
+def test_SingleOPER_OPND():
+    assert not evaluate("5+") == 5
+
+def test_plus():
+    assert evaluate("58+") == 13
+
