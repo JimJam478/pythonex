6f02ea84d496e9b728c2fb8d5e6ab0595ba2860f
Allen <allensreebinoj2010@gmail.com>
2023-11-01 15:52:03 +0530

    GREEN: Test passed for addition operation in postfix evaluation

--- test_eval.py
+++ test_eval.py
@@ -3,9 +3,6 @@ from pyex2 import evaluate
 def test_singleoperand():
     assert evaluate("6") == 6
 
-def test_SingleOPER_OPND():
-    assert not evaluate("5+") == 5
-
 def test_plus():
     assert evaluate("58+") == 13
 
